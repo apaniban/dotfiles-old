3d4b58bcc734b26fda91dcddbf16a69664177aef
Aljon Aniban <aljon@Aljons-MacBook-Pro-3.local>
2016-09-07 12:46:59 +1000

    Add tmux configuration.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc.before vimrc.after zshrc"    # list of files/folders to symlink in homedir
+files="vimrc.before vimrc.after zshrc tmux.conf"    # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
